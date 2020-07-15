db0c4b37c9dc1f3f4b62c34661f0fea3a9fa04ad
Yossapon <68343388+Yossapon0@users.noreply.github.com>
2020-07-15 23:19:29 +0700

    Add files via upload

---
+++ FirstProjeck.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{ 
+	cout << "1st Term of 2020		\\45132114	Wanthanee Prachuabsupakij" << endl; 
+	cout << "---------------------------------------------------------------------------" << endl;
+	cout << "|Subject ID	| Subject name			score (100) | Grade |" << endl;
+	cout << "---------------------------------------------------------------------------" << endl;
+	cout << "|4570034	| Introduction to Computer |	85	|	\"A\"	|" << endl;	
+	cout << "|4580247	| English	           |    74      |	\"B\"	|"<< endl;
+	cout << "|4530001	| Desing Thinking	   |	85	|	\"A\"	|" << endl;
+	cout << "---------------------------------------------------------------------------" << endl;
+	cout << "|	TOTAL	| Subject = 3			   |	Average = 3.87 |" << endl;
+	cout << "================================*===" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "KMUTNB" << endl;
+	return(0);
+}
